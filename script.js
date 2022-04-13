e930af19421aaad677ca588692f20b7c446c8aa0
Khobaib1 <97926066+Khobaib1@users.noreply.github.com>
2022-04-13 18:59:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 document.addEventListener('DOMContentLoaded',  function (){
     document.getElementById('button').onclick = async function(){
     let search = document.getElementById('search').value;
@@ -8,12 +6,27 @@ document.addEventListener('DOMContentLoaded',  function (){
     console.log(api)
     console.log(search)
     const respData = await api.json()
+    let title = respData.items[1].volumeInfo.title
     console.log(respData)
     // console.log(respData.items[1].volumeInfo.authors[1])
-    const title = document.getElementById('title').innerHTML = `Title: ${respData.items[1].volumeInfo.title}`
+    document.getElementById('title').innerHTML = `Title : ${title}`
     const author = document.getElementById('author').innerHTML = `Author : ${respData.items[0].volumeInfo.authors[0]}`
-    const rating = document.getElementById('rating').innerHTML = `Rating : ${respData.items[1].volumeInfo.averageRating}`
+    let rating =  respData.items[1].volumeInfo.averageRating
+    console.log(rating)
+    let image = ""
+    console.log(image)
+    if (rating !== undefined && image !== undefined){
+        document.getElementById('rating').innerHTML = `Rating : ${respData.items[1].volumeInfo.averageRating}`
+        document.getElementById('thumbnail').src = `${respData.items[1].volumeInfo.imageLinks.thumbnail}`
+
+        console.log(rating)
+        console.log(image)
+    }else{
+        document.getElementById('rating').innerHTML = `Rating : ${respData.items[3].volumeInfo.averageRating}`
+        document.getElementById('thumbnail').src = `${respData.items[0].volumeInfo.imageLinks.thumbnail}`
+    }
+    const link = document.getElementById('link').href = `${respData.items[0].volumeInfo.previewLink}`
     const language = document.getElementById('language').innerHTML = `Language : ${respData.items[1].volumeInfo.language}`
     const publishDate = document.getElementById('publishDate').innerHTML = `Publish Date : ${respData.items[0].volumeInfo.publishedDate}`
-    const image = document.getElementById('thumbnail').src = `${respData.items[1].volumeInfo.imageLinks.thumbnail}`
-}})
+    }})
+    
